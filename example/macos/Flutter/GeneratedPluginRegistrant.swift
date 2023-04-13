02a4282a0d7ddf21245a9ba490460be48ce54498
Maruyama Takeshi <tac@sotai.co>
2023-04-13 12:12:10 +0900

    #6 Add isar plugin

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import flutter_bluesky
+import isar_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterBlueskyPlugin.register(with: registry.registrar(forPlugin: "FlutterBlueskyPlugin"))
+  IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
 }
--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <flutter_bluesky/flutter_bluesky_plugin_c_api.h>
+#include <isar_flutter_libs/isar_flutter_libs_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FlutterBlueskyPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterBlueskyPluginCApi"));
+  IsarFlutterLibsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("IsarFlutterLibsPlugin"));
 }
