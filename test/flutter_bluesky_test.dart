7285dc58124419d575a1550fb006808cc9ea278a
Maruyama Takeshi <tac@sotai.co>
2023-04-13 15:29:32 +0900

    #6 Add test.

--- test/flutter_bluesky_test.dart
+++ test/flutter_bluesky_test.dart
@@ -1,6 +1,9 @@
+import 'package:flutter_bluesky/db.dart';
+import 'package:flutter_bluesky/exception.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_bluesky/flutter_bluesky.dart';
 import 'package:flutter_bluesky/flutter_bluesky_platform_interface.dart';
+import 'package:isar/isar.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 class MockFlutterBlueskyPlatform
@@ -13,11 +16,30 @@ class MockFlutterBlueskyPlatform
 void main() {
   final FlutterBlueskyPlatform initialPlatform =
       FlutterBlueskyPlatform.instance;
+  setUp(() async {
+    await Isar.initializeIsarCore(
+      download: true,
+    );
+    db.open();
+  });
 
+  tearDown(() async {
+    db.close();
   });
 
   test('connect', () async {
     FlutterBluesky flutterBlueskyPlugin = FlutterBluesky();
     expect(await flutterBlueskyPlugin.connect(), 200);
+  });
+
+  test('login email', () async {
+    FlutterBluesky flutterBlueskyPlugin =
+        FlutterBluesky(provider: "http://localhost:2583");
+    try {
+      int status = await flutterBlueskyPlugin.login("foo@bar.com", "hoge");
+      expect(status, 200);
+    } on APIPostException catch (e) {
+      fail(e.message);
+    }
   });
 }
