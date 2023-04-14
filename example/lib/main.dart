3e2be404727dc5158371d7dab95ae903741ad8e5
Maruyama Takeshi <tac@sotai.co>
2023-04-14 15:59:16 +0900

    #3 Fix App title.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -35,7 +35,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Flutter Bluesky example App'),
         ),
         body: Center(
           child: serverInfo(),
