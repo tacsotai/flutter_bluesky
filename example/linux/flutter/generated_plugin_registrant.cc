47a0ddde77514f4e324c93ad68962fa468fd3101
Maruyama Takeshi <tac@sotai.co>
2023-04-14 13:59:24 +0900

    #8 Remove isar from pubspec.yaml

--- example/linux/flutter/generated_plugin_registrant.cc
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -7,13 +7,9 @@
 #include "generated_plugin_registrant.h"
 
 #include <flutter_bluesky/flutter_bluesky_plugin.h>
-#include <isar_flutter_libs/isar_flutter_libs_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) flutter_bluesky_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterBlueskyPlugin");
   flutter_bluesky_plugin_register_with_registrar(flutter_bluesky_registrar);
-  g_autoptr(FlPluginRegistrar) isar_flutter_libs_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "IsarFlutterLibsPlugin");
-  isar_flutter_libs_plugin_register_with_registrar(isar_flutter_libs_registrar);
 }
--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import flutter_bluesky
-import isar_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterBlueskyPlugin.register(with: registry.registrar(forPlugin: "FlutterBlueskyPlugin"))
-  IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
 }
