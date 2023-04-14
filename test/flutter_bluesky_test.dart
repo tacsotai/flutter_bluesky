4d6e1ddde3466e0cb2abab8d9607945399103da7
Maruyama Takeshi <tac@sotai.co>
2023-04-14 11:20:17 +0900

    #4 Add test code.

--- test/flutter_bluesky_test.dart
+++ test/flutter_bluesky_test.dart
@@ -42,6 +42,45 @@ void main() {
       expect(res.item1, 200);
     } on Exception catch (e) {
       fail(e.toString());
+  test('connect', () async {
+    Tuple2 res = await plugin.connect();
+    expect(res.item1, 200);
+    Map expected = {
+      "availableUserDomains": [".test", ".dev.bsky.dev"],
+      "inviteCodeRequired": false,
+      "links": {}
+    };
+    expect(res.item2, expected);
+  });
+
+  test('register', () async {
+    Tuple2 res = await plugin.register("foo@bar.com", "hoge.test", "password");
+    if (!(res.item1 == 400 &&
+        res.item2["message"] == "Handle already taken: hoge.test")) {
+      Tuple2 res2 =
+          await plugin.register("foo@bar.com", "hoge.test", "password");
+      expect(res2.item1, 200);
     }
+  });
+
+  test('register NG, handle domain is not available', () async {
+    Tuple2 res = await plugin.register("foo@bar.com", "handle", "password");
+    expect(res.item1, 400);
+  });
+
+  test('login OK', () async {
+    Tuple2 res = await plugin.login("foo@bar.com", "password");
+    expect(res.item1, 200);
+  });
+
+  test('login failure', () async {
+    Tuple2 res = await plugin.login("foo@bar.com", "hoge");
+    expect(res.item1, 401);
+  });
+
+  test('timeline', () async {
+    await plugin.login("foo@bar.com", "password");
+    Tuple2 res = await plugin.timeline();
+    expect(res.item1, 200);
   });
 }
