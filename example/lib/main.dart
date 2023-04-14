810a3b1c4e4ad4cd8e50de945a9c59f1edbe24e2
Maruyama Takeshi <tac@sotai.co>
2023-04-14 15:57:41 +0900

    Make draft plugin client . #3

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'package:flutter_bluesky/api/session.dart';
+
 import 'package:tuple/tuple.dart';
 import 'package:flutter_bluesky/flutter_bluesky.dart';
 
@@ -15,26 +18,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String message = "Connecting ..";
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    // defalt provider = 'https://bsky.social'
+    _controller.text = defaultProvider;
   }
 
-  Future<void> initPlatformState() async {
-    final plugin = FlutterBluesky();
-    final String provider = plugin.getProvider();
-    Tuple2 res = await plugin.connect();
-    bool result = res.item1 == 200;
+  final _formKey = GlobalKey<FormState>();
+  final _controller = TextEditingController();
 
-    if (!mounted) return;
-
-    setState(() {
-      String prefix = result ? "" : "Not ";
-      message = '${prefix}Connected to $provider';
-    });
-  }
+  String message = "No connected to any provider yet.";
 
   @override
   Widget build(BuildContext context) {
@@ -44,9 +38,50 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text(message),
+          child: serverInfo(),
         ),
       ),
     );
+  }
+
+  Widget serverInfo() {
+    return ListView(
+      padding: const EdgeInsets.all(10),
+      children: [
+        form(),
+        Center(child: Text(message)),
+      ],
+    );
+  }
+
+  Widget form() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            controller: _controller,
+            decoration: const InputDecoration(labelText: "Provider"),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () async {
+                final plugin = FlutterBluesky(provider: _controller.text);
+                Tuple2 res = await plugin.connect();
+                setState(() {
+                  set(res);
+                });
+              },
+              child: const Text('Connect'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void set(Tuple2 res) async {
+    message = json.encode(res.item2);
   }
 }
