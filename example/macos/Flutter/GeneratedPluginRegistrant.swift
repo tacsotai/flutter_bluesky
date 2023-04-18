d67484e7772f4dcb9190072c3de9591a22e7cd7b
Maruyama Takeshi <tac@sotai.co>
2023-04-18 14:27:16 +0900

    #9 fix example pubspec

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import flutter_bluesky
+import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterBlueskyPlugin.register(with: registry.registrar(forPlugin: "FlutterBlueskyPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
