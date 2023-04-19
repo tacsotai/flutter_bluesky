57c7ffb98e8fd5a81497b867cedab7ace15581a8
Maruyama Takeshi <tac@sotai.co>
2023-04-19 16:56:27 +0900

    #12 Make home draft

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bluesky/flutter_bluesky.dart';
 import 'package:flutter_bluesky/screen.dart';
+import 'package:flutter_bluesky/screen/parts/connecting.dart';
+import 'package:flutter_bluesky/screen/parts/timelines.dart';
+import 'package:tuple/tuple.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -20,13 +24,38 @@ class HomeScreen extends State<Home> with Base {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: appBar(context),
-        body: lists(context),
+        body: timeline(context),
         floatingActionButton: post(context),
         bottomNavigationBar: menu(context));
   }
 
+  // https://zenn.dev/sqer/articles/db20a4d735fb7e5928ba
+  Widget timeline(BuildContext context) {
+    return FutureBuilder(
+      future: plugin.timeline(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          // TODO consider below coding
+          if (snapshot.hasError) {
+            return Text("Error: ${snapshot.error}");
+          }
+          if (!snapshot.hasData) {
+            return const Text("Not found the data.");
+          }
+          return lists(context, res: snapshot.data);
+        } else {
+          return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
+
   @override
-    return [];
   List<Widget> listview(BuildContext context, {Tuple2? res}) {
+    if (res!.item1 != 200) {
+      return Connecting(context).listview();
+    } else {
+      return Timelines(context, res.item2).listview();
+    }
   }
 }
