d4590c85f890d60437342f63006784c94d29ecbb
Maruyama Takeshi <tac@sotai.co>
2023-04-25 08:13:53 +0900

    Split InfinityListView from home #20

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bluesky/flutter_bluesky.dart';
 import 'package:flutter_bluesky/screen.dart';
-import 'package:flutter_bluesky/screen/parts/refresh/material.dart';
-import 'package:flutter_bluesky/screen/parts/timeline.dart';
+
 import 'package:flutter_bluesky/api/model/feed.dart';
+import 'package:flutter_bluesky/screen/home/infinity_list_view.dart';
 import 'package:tuple/tuple.dart';
-import 'package:flutter/rendering.dart';
 
 // https://blog.flutteruniv.com/flutter-infinity-scroll/
 // https://api.flutter.dev/flutter/material/SliverAppBar-class.html
@@ -69,122 +68,5 @@ class HomeScreen extends State<Home> with Frame {
     } else {
       feeds.addAll(list);
     }
-  }
-}
-
-class InfinityListView extends StatefulWidget {
-  final List<Feed> feeds;
-  final Future<void> Function(bool) getFeeds;
-  final void Function(bool) hide;
-
-  const InfinityListView({
-    Key? key,
-    required this.feeds,
-    required this.getFeeds,
-    required this.hide,
-  }) : super(key: key);
-
-  @override
-  State<InfinityListView> createState() => _InfinityListViewState();
-}
-
-class _InfinityListViewState extends State<InfinityListView> {
-  late ScrollController _scrollController;
-  bool isHidden = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _scrollController = ScrollController();
-    _scrollController.addListener(() async {
-      if (_scrollController.position.userScrollDirection ==
-          ScrollDirection.reverse) {
-        if (!isHidden) {
-          widget.hide(true);
-          isHidden = true;
-        }
-      } else {
-        if (isHidden) {
-          widget.hide(false);
-          isHidden = false;
-        }
-      }
-      if (_scrollController.position.pixels >=
-              _scrollController.position.maxScrollExtent * 0.95 &&
-          !_isLoading) {
-        _isLoading = true;
-
-        await widget.getFeeds(false);
-
-        setState(() {
-          _isLoading = false;
-        });
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  bool _isLoading = false;
-
-  Widget appBar(BuildContext context) {
-    return SliverAppBar(
-      floating: true,
-      flexibleSpace: FlexibleSpaceBar(
-        title: Text(Home.screen.name),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return CustomScrollView(
-      physics: const BouncingScrollPhysics(
-        parent: AlwaysScrollableScrollPhysics(),
-      ),
-      semanticChildCount: widget.feeds.length,
-      controller: _scrollController,
-      slivers: [
-        appBar(context),
-        MaterialSliverRefreshControl(
-          onRefresh: () async {
-            await widget.getFeeds(true);
-            setState(() {});
-          },
-        ),
-        SliverList(
-            delegate: SliverChildBuilderDelegate(
-          childCount: widget.feeds.length + 1,
-          (BuildContext context, int index) {
-            if (widget.feeds.length == index) {
-              widget.getFeeds(false);
-              return const SizedBox(
-                height: 50,
-                child: Center(child: CircularProgressIndicator()),
-              );
-            }
-            return _build(widget.feeds[index]);
-          },
-        ))
-      ],
-    );
-  }
-
-  Widget _build(Feed feed) {
-    Timeline line = Timeline(context, feed);
-    return Column(children: [
-      Container(
-        margin: const EdgeInsets.all(10),
-        child: Padding(
-          padding: const EdgeInsets.all(5),
-          child: line.build(),
-        ),
-      ),
-      const Divider(height: 0.5)
-    ]);
   }
 }
