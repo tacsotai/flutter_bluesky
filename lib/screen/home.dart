b69892d3966ea8a1bdf4a10df2d502640bb96073
Maruyama Takeshi <tac@sotai.co>
2023-04-26 08:41:06 +0900

    Fix the timeline getting latest data. #21

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -55,9 +55,40 @@ class HomeScreen extends State<Home> with Frame {
 
   Future<void> getFeeds(bool insert) async {
     Tuple2 res = await plugin.timeline(cursor: insert ? null : cursor);
-    cursor = res.item2["cursor"];
+    String resCursor = res.item2["cursor"];
     List<Feed> list = _getList(res.item2["feed"]);
-    insert ? feeds.insertAll(0, list) : feeds.addAll(list);
+    makeFeeds(insert, resCursor, list);
+    cursor = resCursor;
+  }
+
+  // TODO unit test
+  void makeFeeds(bool insert, String resCursor, List<Feed> list) {
+    if (cursor == null) {
+      feeds.addAll(list);
+    } else if (cursor != resCursor) {
+      int current = int.parse(cursor!.split('::')[0]);
+      int latest = int.parse(resCursor.split('::')[0]);
+      if (latest < current) {
+        feeds.addAll(list);
+      } else {
+        insertFeeds(insert, latest, list);
+      }
+    } else {
+      // cursor == resCursor case, Do nothing.
+    }
+  }
+
+  void insertFeeds(bool insert, int latest, List<Feed> list) {
+    if (insert) {
+      DateTime cursorDate = DateTime.fromMillisecondsSinceEpoch(latest);
+      List<Feed> inserts = [];
+      for (Feed feed in list) {
+        if (feed.post.record.createdAt.isAfter(cursorDate)) {
+          inserts.add(feed);
+        }
+      }
+      feeds.insertAll(0, inserts);
+    }
   }
 
   List<Feed> _getList(List feeds) {
