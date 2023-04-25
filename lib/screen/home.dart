49efd17af00a50292f05b482dc7bc6bce1cbd679
Maruyama Takeshi <tac@sotai.co>
2023-04-26 08:48:45 +0900

    #21 Separate code

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bluesky/flutter_bluesky.dart';
 import 'package:flutter_bluesky/screen.dart';
-
-import 'package:flutter_bluesky/api/model/feed.dart';
 import 'package:flutter_bluesky/screen/home/infinity_list_view.dart';
-import 'package:tuple/tuple.dart';
+import 'package:flutter_bluesky/screen/home/timeline.dart';
 
 // https://blog.flutteruniv.com/flutter-infinity-scroll/
 // https://api.flutter.dev/flutter/material/SliverAppBar-class.html
@@ -20,6 +17,8 @@ class Home extends StatefulWidget {
 }
 
 class HomeScreen extends State<Home> with Frame {
+  final Timeline _timeline = Timeline();
+
   @override
   Widget build(BuildContext context) {
     return scaffold(
@@ -33,7 +32,7 @@ class HomeScreen extends State<Home> with Frame {
   @override
   Widget body() {
     return FutureBuilder(
-        future: getFeeds(false),
+        future: _timeline.getFeeds(false),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
@@ -42,60 +41,11 @@ class HomeScreen extends State<Home> with Frame {
             return Text("Error: ${snapshot.error}");
           } else {
             return InfinityListView(
-              feeds: feeds,
-              getFeeds: getFeeds,
+              feeds: _timeline.feeds,
+              getFeeds: _timeline.getFeeds,
               hide: widget.hideBottom,
             );
           }
         });
-  }
-
-  String? cursor;
-  List<Feed> feeds = [];
-
-  Future<void> getFeeds(bool insert) async {
-    Tuple2 res = await plugin.timeline(cursor: insert ? null : cursor);
-    String resCursor = res.item2["cursor"];
-    List<Feed> list = _getList(res.item2["feed"]);
-    makeFeeds(insert, resCursor, list);
-    cursor = resCursor;
-  }
-
-  // TODO unit test
-  void makeFeeds(bool insert, String resCursor, List<Feed> list) {
-    if (cursor == null) {
-      feeds.addAll(list);
-    } else if (cursor != resCursor) {
-      int current = int.parse(cursor!.split('::')[0]);
-      int latest = int.parse(resCursor.split('::')[0]);
-      if (latest < current) {
-        feeds.addAll(list);
-      } else {
-        insertFeeds(insert, latest, list);
-      }
-    } else {
-      // cursor == resCursor case, Do nothing.
-    }
-  }
-
-  void insertFeeds(bool insert, int latest, List<Feed> list) {
-    if (insert) {
-      DateTime cursorDate = DateTime.fromMillisecondsSinceEpoch(latest);
-      List<Feed> inserts = [];
-      for (Feed feed in list) {
-        if (feed.post.record.createdAt.isAfter(cursorDate)) {
-          inserts.add(feed);
-        }
-      }
-      feeds.insertAll(0, inserts);
-    }
-  }
-
-  List<Feed> _getList(List feeds) {
-    List<Feed> list = [];
-    for (var element in feeds) {
-      list.add(Feed(element));
-    }
-    return list;
   }
 }
---
+++ lib/screen/home/timeline.dart
@@ -0,0 +1,54 @@
+import 'package:flutter_bluesky/flutter_bluesky.dart';
+import 'package:flutter_bluesky/api/model/feed.dart';
+import 'package:tuple/tuple.dart';
+
+class Timeline {
+  String? cursor;
+  List<Feed> feeds = [];
+
+  Future<void> getFeeds(bool insert) async {
+    Tuple2 res = await plugin.timeline(cursor: insert ? null : cursor);
+    String resCursor = res.item2["cursor"];
+    List<Feed> list = _getList(res.item2["feed"]);
+    makeFeeds(insert, resCursor, list);
+    cursor = resCursor;
+  }
+
+  // TODO unit test
+  void makeFeeds(bool insert, String resCursor, List<Feed> list) {
+    if (cursor == null) {
+      feeds.addAll(list);
+    } else if (cursor != resCursor) {
+      int current = int.parse(cursor!.split('::')[0]);
+      int latest = int.parse(resCursor.split('::')[0]);
+      if (latest < current) {
+        feeds.addAll(list);
+      } else {
+        insertFeeds(insert, latest, list);
+      }
+    } else {
+      // cursor == resCursor case, Do nothing.
+    }
+  }
+
+  void insertFeeds(bool insert, int latest, List<Feed> list) {
+    if (insert) {
+      DateTime cursorDate = DateTime.fromMillisecondsSinceEpoch(latest);
+      List<Feed> inserts = [];
+      for (Feed feed in list) {
+        if (feed.post.record.createdAt.isAfter(cursorDate)) {
+          inserts.add(feed);
+        }
+      }
+      feeds.insertAll(0, inserts);
+    }
+  }
+
+  List<Feed> _getList(List feeds) {
+    List<Feed> list = [];
+    for (var element in feeds) {
+      list.add(Feed(element));
+    }
+    return list;
+  }
+}
