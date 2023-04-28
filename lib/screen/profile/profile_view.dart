b544fc8b11f48fee79be5ae502a4b0dab2118fed
Maruyama Takeshi <tac@sotai.co>
2023-04-28 13:10:49 +0900

    #18 Fix profile view

--- lib/screen/profile/profile_view.dart
+++ lib/screen/profile/profile_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bluesky/api/model/actor.dart';
 import 'package:flutter_bluesky/screen/base.dart';
+import 'package:flutter_bluesky/screen/parts/adjuser.dart';
 import 'package:flutter_bluesky/screen/parts/scroll/feed_scroll.dart';
 import 'package:flutter_bluesky/screen/data/manager.dart';
+import 'package:flutter_bluesky/screen/parts/timeline/header.dart';
 
 class ProfileView extends StatefulWidget {
   final ProfileDataManager manager;
@@ -45,18 +48,73 @@ class _ProfileViewState extends State<ProfileView> with FeedScroll {
 
   @override
   List<Widget> get slivers => [
-        const SliverToBoxAdapter(
-          child: ColoredBox(
-            color: Colors.blueGrey,
-            child: SizedBox(
-              height: 100,
-              width: double.infinity,
-              child: Center(
-                child: Text('Header'),
-              ),
-            ),
-          ),
+        SliverToBoxAdapter(
+          child: header,
         ),
         sliverList
       ];
+
+  Widget get header {
+    return Column(
+      children: [colorBox, info, const Divider(height: 0.5)],
+    );
+  }
+
+  Widget get colorBox {
+    return ColoredBox(
+      color: Theme.of(context).colorScheme.primary,
+      child: const SizedBox(
+        height: 100,
+        width: double.infinity,
+      ),
+    );
+  }
+
+  Widget get info {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: SizedBox(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: prof,
+          ),
+        ));
+  }
+
+  List<Widget> get prof {
+    return [
+      displayName(widget.manager.holder.detail, 28),
+      handle(widget.manager.holder.detail),
+      sizeBox,
+      counts,
+      sizeBox,
+      Text(widget.manager.holder.detail.description),
+      sizeBox,
+    ];
+  }
+
+  Widget get counts {
+    ProfileViewDetailed detail = widget.manager.holder.detail;
+    return Row(
+      children: [
+        count(widget.manager.holder.detail.followersCount, 'followers'),
+        sizeBox,
+        count(widget.manager.holder.detail.followsCount, 'following'),
+        sizeBox,
+        count(widget.manager.holder.detail.postsCount, 'posts'),
+      ],
+    );
+  }
+
+  Widget count(int count, String postfix) {
+    return Row(children: [
+      bold(count),
+      Text(postfix, style: const TextStyle(color: Colors.grey)),
+    ]);
+  }
+
+  Widget bold(int count) {
+    return Text(count.toString(),
+        style: const TextStyle(fontWeight: FontWeight.bold));
+  }
 }
