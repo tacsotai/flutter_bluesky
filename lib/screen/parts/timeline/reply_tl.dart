5914760f9d2adf6b8c57a3856460b3d53235f92e
Maruyama Takeshi <tac@sotai.co>
2023-05-10 12:59:02 +0900

    Make Reply view on timeline #14

--- lib/screen/parts/timeline/reply_tl.dart
+++ lib/screen/parts/timeline/reply_tl.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bluesky/api/model/feed.dart';
 import 'package:flutter_bluesky/screen/parts/timeline/common.dart';
+import 'package:flutter_bluesky/screen/parts/adjuser.dart';
+import 'package:flutter_bluesky/screen/parts/avator.dart';
 
 class ReplyTimeline extends CommonTimeline {
   @override
@@ -8,6 +10,48 @@ class ReplyTimeline extends CommonTimeline {
     if (feed.reply == null) {
       return null;
     }
-    return const Text("Reply");
+    Post parent = feed.reply!.parent;
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        replyAvator(parent),
+        sizeBox,
+        contentFrame(context, feed),
+      ],
+    );
+  }
+
+  Widget replyAvator(Post post) {
+    // TODO height is solid, so make it flexible.
+    // debugPrint("post.record.text.length: ${post.record.text.length}");
+    return Column(children: [
+      avator(post.author.avatar),
+      Container(
+          width: 1,
+          height: 70,
+          decoration: const BoxDecoration(
+            color: Colors.grey,
+          ))
+    ]);
+  }
+
+  @override
+  List<Widget> content(BuildContext context, Feed feed) {
+    Post parent = feed.reply!.parent;
+    return [
+      header(context, parent),
+      replyHeader(feed.post),
+      body(parent),
+      footer(context, parent),
+    ];
+  }
+
+  Widget replyHeader(Post original) {
+    return Row(children: [
+      const Icon(Icons.reply, color: Colors.grey),
+      Expanded(
+          child: Text('Reply to ${original.author.displayName}',
+              style: const TextStyle(color: Colors.grey)))
+    ]);
   }
 }
