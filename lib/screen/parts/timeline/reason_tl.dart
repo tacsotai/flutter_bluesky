61878e3bc72a053ba62c5a9ca03c16fced2bbf97
Maruyama Takeshi <tac@sotai.co>
2023-05-10 15:37:28 +0900

    Make Repost View #24

--- lib/screen/parts/timeline/reason_tl.dart
+++ lib/screen/parts/timeline/reason_tl.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bluesky/api/model/feed.dart';
+import 'package:flutter_bluesky/screen/parts/adjuser.dart';
 import 'package:flutter_bluesky/screen/parts/timeline/common.dart';
 
 class ReasonTimeline extends CommonTimeline {
   @override
   Widget? build(BuildContext context, Feed feed) {
-    if (feed.reply == null) {
+    if (feed.reason == null) {
       return null;
     }
-    return const Text("Reason");
+    return Row(
+      children: [
+        const SizedBox(width: 60),
+        const Icon(Icons.repeat, color: Colors.grey, size: 14),
+        sizeBox,
+        Text('Reposted by ${feed.reason!.by.displayName}',
+            style: const TextStyle(
+                color: Colors.grey, fontWeight: FontWeight.bold))
+      ],
+    );
   }
 }
