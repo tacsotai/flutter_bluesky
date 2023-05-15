b6d16cbdc21bba52546f290318ff0aa72f87e160
Maruyama Takeshi <tac@sotai.co>
2023-05-15 16:23:22 +0900

    #25 Adapt left resources.

--- example/lib/sample_timeline.dart
+++ example/lib/sample_timeline.dart
@@ -1,31 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bluesky/api/model/feed.dart';
-import 'package:flutter_bluesky/screen/parts/adjuser.dart';
 import 'package:flutter_bluesky/screen/parts/timeline/common.dart';
-import 'package:flutter_bluesky/screen/parts/avator.dart';
+import 'package:flutter_bluesky/screen/parts/timeline/post_tl.dart';
 
-class SamplePostTimeline extends CommonTimeline {
+class SamplePostTimeline extends PostTL {
   @override
-  Widget build(BuildContext context, Feed feed) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        avator(feed.post.author.avatar),
-        sizeBox,
-        contentFrame(context, feed)
-      ],
-    );
-  }
-
-  @override
-  Widget body(BuildContext context, Post post) {
-    return Row(
-      children: [
-        Expanded(
-            child: Text(
-          'pluggable ${post.record.text}',
-        )),
-      ],
-    );
+  Widget build() {
+    post.record.text = 'pluggable ${post.record.text}';
+    return postTL(post);
   }
 }
