61c5f8da0408180b34a3d41145bf8c6d3670c450
Maruyama Takeshi <tac@sotai.co>
2023-05-26 18:36:21 +0900

    #54 comment out the pluggable code.

--- example/lib/sample_timeline.dart
+++ example/lib/sample_timeline.dart
@@ -5,7 +5,7 @@ import 'package:flutter_bluesky/screen/parts/timeline/post_tl.dart';
 class SamplePostTimeline extends PostTL {
   @override
   Widget build(BuildContext context) {
-    post.record.text = 'pluggable ${post.record.text}';
+    // post.record.text = 'pluggable ${post.record.text}';
     return postTL(context, post);
   }
 }
