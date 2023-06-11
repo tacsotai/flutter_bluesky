2be6972f55338c9c68b6c1afa6915a1355148603
Maruyama Takeshi <tac@sotai.co>
2023-06-11 17:56:25 +0900

    #72 Fix model

--- lib/api/model/notification.dart
+++ lib/api/model/notification.dart
@@ -2,17 +2,25 @@ import 'package:flutter_bluesky/api/model/actor.dart';
 
 class ListNotifications {
   String? cursor;
-  List notifications = [];
+  List notificationList = [];
 
   ListNotifications(Map body)
       : cursor = body["cursor"],
-        notifications = body["notifications"];
+        notificationList = body["notifications"];
+
+  List<Notification> get notifications {
+    List<Notification> list = [];
+    for (var map in notificationList) {
+      list.add(Notification(map));
+    }
+    return list;
+  }
 }
 
 class Notification {
   final String uri;
   final String cid;
-  final ProfileView author;
+  final ProfileViewBasic author;
   final String reason;
   String? reasonSubject;
   // currently, no need to know detail.
@@ -23,7 +31,7 @@ class Notification {
   Notification(Map map)
       : uri = map["uri"],
         cid = map["cid"],
-        author = ProfileView(map["author"]),
+        author = ProfileViewBasic(map["author"]),
         reason = map["reason"],
         reasonSubject = map["reasonSubject"],
         record = map["record"],
