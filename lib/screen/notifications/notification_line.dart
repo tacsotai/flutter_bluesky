fdd1ab279e4df0c09fee5a0564fce34d3e5c61e9
Maruyama Takeshi <tac@sotai.co>
2023-06-14 11:46:32 +0900

    #72 creating notification line

--- lib/screen/notifications/notification_line.dart
+++ lib/screen/notifications/notification_line.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bluesky/api/model/notification.dart' as notice;
 import 'package:flutter_bluesky/screen/parts/adjuser.dart';
+import 'package:flutter_bluesky/screen/parts/avatar.dart';
 import 'package:flutter_bluesky/screen/parts/timeline/common.dart';
 
-NotificationContent? customNotificationContent;
+Notice? customNotice;
 
 class NotificationsLine extends StatefulWidget {
   final notice.Notification notification;
@@ -16,21 +17,57 @@ class NotificationsLine extends StatefulWidget {
 class NotificationsLineScreen extends State<NotificationsLine> {
   @override
   Widget build(BuildContext context) {
-    return padding(paddingLR([
-      Text("TODO")
-    ], [
-      contet,
-    ]));
-  }
-
-  Widget get contet {
-    NotificationContent sc = customNotificationContent ?? NotificationContent();
-    return sc.build(this, widget.notification);
+    Notice notice = customNotice ?? Notice();
+    return padding(notice.build(this, widget.notification));
   }
 }
 
-class NotificationContent {
+class Notice {
   Widget build(State state, notice.Notification notification) {
-    return Text("notification TODO");
+    switch (notification.reason) {
+      case "follow":
+        return follow(state, notification);
+      case "reply":
+        return reply(state, notification);
+      case "like":
+        return like(state, notification);
+      case "repost":
+        return repost(state, notification);
+      default:
+        return error(state, notification);
+    }
+  }
+
+  Widget follow(State state, notice.Notification notification) {
+    return paddingLR([
+      const Icon(Icons.favorite_sharp, color: Colors.pink)
+    ], [
+      Column(
+        children: [
+          Avatar(state.context).net(notification.author).profile,
+          displayName(notification.author),
+        ],
+      )
+    ]);
+  }
+
+  Widget reply(State state, notice.Notification notification) {
+    return paddingLR([
+      Avatar(state.context).net(notification.author).profile
+    ], [
+      Text(notification.record["text"]),
+    ]);
+  }
+
+  Widget like(State state, notice.Notification notification) {
+    return Text("notification.reason ${notification.reason}");
+  }
+
+  Widget repost(State state, notice.Notification notification) {
+    return Text("notification.reason ${notification.reason}");
+  }
+
+  Widget error(State state, notice.Notification notification) {
+    return Text("Error: Not implementd for '${notification.reason}'");
   }
 }
