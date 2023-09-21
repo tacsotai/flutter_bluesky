149bf0c7646c250255610111c3a4e0fdcede7f61
Maruyama Takeshi <tac@sotai.co>
2023-09-21 11:19:44 +0900

    Revert "#125 Remove Timer from Base."
    This reverts commit 3ed3d635353570fe6315656486e086db883cbe51.

--- lib/screen/base.dart
+++ lib/screen/base.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bluesky/flutter_bluesky.dart';
 import 'package:flutter_bluesky/screen.dart';
@@ -35,6 +37,9 @@ class BaseScreen extends State<Base> with SingleTickerProviderStateMixin {
       for (var pluggable in pluggables) {
         pluggable.init();
       }
+      Timer(const Duration(milliseconds: 1000), () {
+        setState(() {});
+      });
     }
   }
 
