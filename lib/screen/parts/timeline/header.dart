a8358ebfb72b144f94ff226ecd3ebd97aadc53dc
Maruyama Takeshi <tac@sotai.co>
2023-10-17 16:52:39 +0900

    Make it possible to be pluggable for Header #157

--- lib/screen/parts/timeline/header.dart
+++ lib/screen/parts/timeline/header.dart
@@ -5,6 +5,8 @@ import 'package:flutter_bluesky/screen/parts/timeline/common.dart';
 import 'package:flutter_bluesky/util/datetime_util.dart';
 import 'package:tuple/tuple.dart';
 
+Header? customHeder;
+
 class Header extends StatelessWidget {
   final ProfileViewBasic author;
   final DateTime createdAt;
@@ -12,6 +14,15 @@ class Header extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (customHeder != null) {
+      return customHeder!.content(context, author, createdAt);
+    }
+    return content(context, author, createdAt);
+  }
+
+  // TO BE OVER WRITE
+  Widget content(
+      BuildContext context, ProfileViewBasic author, DateTime createdAt) {
     return padding(
       lr(displayNameHandle(author), when(context), const Tuple2(5, 2)),
       left: 0,
