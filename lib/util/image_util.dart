2cd38891166c265547b35e56a35474c776f17b50
Maruyama Takeshi <tac@sotai.co>
2023-11-17 15:44:35 +0900

    Consider picture quality #179

--- lib/util/image_util.dart
+++ lib/util/image_util.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:file_picker/file_picker.dart';
@@ -35,32 +36,55 @@ class ImageUtil {
   }
 }
 
+// https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/embed/images.json
+const int maxSize = 1000000; // 1MB
+
 class ImageFile {
-  static const maxWidth = 450;
   final PlatformFile file;
   final Uint8List origin;
   final String? mimeType;
+
   ImageFile(this.file)
       : origin = file.bytes ?? File(file.path!).readAsBytesSync(),
         mimeType = ImageUtil.exts[path.extension(file.name).substring(1)];
 
   Uint8List get bytes {
-    img.Image? original = img.decodeImage(origin);
-    if (original!.width > maxWidth) {
-      img.Image resized = img.copyResize(original, width: maxWidth);
-      // debugPrint("resized!.width: ${resized.width}");
-      return encode(resized);
+    if (origin.length > maxSize) {
+      return resize(origin);
     } else {
       return origin;
     }
   }
 
+  Uint8List resize(Uint8List bytes, {int count = 0}) {
+    img.Image image = img.decodeImage(bytes)!;
+    image = img.copyResize(image, width: width(bytes, image, count));
+    Uint8List resized = encode(image);
+    // debugPrint("resized.length: ${resized.length}");
+    // check the size
+    if (resized.length > maxSize) {
+      count++;
+      resized = resize(resized, count: count);
+    }
+    return resized;
+  }
+
+  int width(Uint8List bytes, img.Image image, int count) {
+    double ratio = maxSize / bytes.length;
+    ratio = count < 2 ? sqrt(ratio) : ratio;
+    // debugPrint("count: $count");
+    // debugPrint("ratio: $ratio");
+    return (image.width * ratio).toInt();
+  }
+
   Uint8List encode(img.Image image) {
     switch (mimeType) {
       case "image/jpeg":
-        return Uint8List.fromList(img.encodeJpg(image));
+        // best trade off between speed and quality, may be
+        return img.encodeJpg(image, quality: 85);
       case "image/png":
-        return Uint8List.fromList(img.encodePng(image));
+        // https://github.com/brendan-duncan/archive/blob/main/lib/src/zlib/deflate.dart#L11
+        return img.encodePng(image, level: 9); // 9 = BEST_COMPRESSION
       default:
         throw UnimplementedError("Only Jpeg and Png are supported.");
     }
