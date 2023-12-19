d2c452d0690088a777c04e9ac90648e06e7fda82
Maruyama Takeshi <tac@sotai.co>
2023-12-19 10:44:33 +0900

    Localization to validate a signup handle #198

--- lib/util/login_util.dart
+++ lib/util/login_util.dart
@@ -1,8 +1,14 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_bluesky/screen/parts/timeline/common.dart';
 
+// https://github.com/tacsotai/atproto/blob/local/packages/identifier/src/handle.ts
 const emailTaken = "Email already taken: ";
 const handleTaken = "Handle already taken: ";
+const handleTooLong = "Handle too long";
+const handleTooShort = "Handle too short";
+// other; same message for this project.
+// const handleInvalid = "Invalid characters in handle";
+// const handleDotContain = "Input/handle must be a valid handle";
 
 // It can change behaviors.
 LoginUtil? loginUtil;
@@ -17,8 +23,13 @@ class LoginUtil {
       return emailMsg(message.substring(emailTaken.length));
     } else if (message.startsWith(handleTaken)) {
       return handleMsg(message.substring(handleTaken.length));
+    } else if (message.contains(handleTooLong)) {
+      return longMsg;
+    } else if (message.contains(handleTooShort)) {
+      return shortMsg;
+    } else {
+      return inavalidMsg;
     }
-    return message;
   }
 
   String emailMsg(String email) {
@@ -28,5 +39,17 @@ class LoginUtil {
   String handleMsg(String handle) {
     handle = withoutDomain(handle);
     return tr("signup.handle.taken", args: [handle]);
+  }
+
+  String get longMsg {
+    return tr("signup.handle.long");
+  }
+
+  String get shortMsg {
+    return tr("signup.handle.short");
+  }
+
+  String get inavalidMsg {
+    return tr("signup.handle.invalid");
   }
 }
