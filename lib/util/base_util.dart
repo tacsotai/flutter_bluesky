0f245861bfbb7cceb43d849177cc0762b60d4a4b
Maruyama Takeshi <tac@sotai.co>
2023-12-28 11:38:12 +0900

    Change path after login #202

--- lib/util/base_util.dart
+++ lib/util/base_util.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bluesky/screen/base.dart';
 
 void pushBase(BuildContext context, {int selectedIndex = 0}) {
   Navigator.of(context).pushReplacement(MaterialPageRoute(
+    settings: RouteSettings(name: Base.route),
     builder: (context) => Base(selectedIndex: selectedIndex),
   ));
 }
