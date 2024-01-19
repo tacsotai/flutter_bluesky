17b5be6db247bd6466ef5bc698074b00bc56f9fb
Maruyama Takeshi <tac@sotai.co>
2024-01-19 16:12:52 +0900

    Add labels and tags to model #206

--- lib/api/model/embed.dart
+++ lib/api/model/embed.dart
@@ -131,6 +131,8 @@ class Record {
   DateTime createdAt;
   List? langs;
   List<Facet>? facets;
+  SelfLabels? labels;
+  List<String>? tags;
   RecordReply? reply;
   RecordEmbed? embed;
   Record(Map map)
@@ -138,9 +140,35 @@ class Record {
         type = map["\$type"],
         langs = map["langs"],
         facets = map["facets"] == null ? null : Facet.list(map["facets"]),
+        labels = map["labels"] == null ? null : SelfLabels(map["labels"]),
+        tags = map["tags"],
         reply = map["reply"] == null ? null : RecordReply(map["reply"]),
         embed = map["embed"] == null ? null : RecordEmbed(map["embed"]),
         createdAt = DateTime.parse((map["createdAt"]));
+}
+
+class SelfLabels {
+  final String type;
+  final List<SelfLabel> values;
+
+  SelfLabels(Map map)
+      : type = map["type"],
+        values = SelfLabel.list(map["values"]);
+}
+
+class SelfLabel {
+  final String val;
+  final Map? content;
+
+  SelfLabel(this.val, this.content);
+
+  static List<SelfLabel> list(List values) {
+    List<SelfLabel> selfLabels = [];
+    for (Map map in values) {
+      selfLabels.add(SelfLabel(map["val"], map["content"]));
+    }
+    return selfLabels;
+  }
 }
 
 class RecordReply {
