0b2949db3ca7c1a0c80e5becd3d5d86fc883c286
Maruyama Takeshi <tac@sotai.co>
2024-01-26 08:26:06 +0900

    #223 Navigate to search at no follows

--- lib/login.dart
+++ lib/login.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bluesky/flutter_bluesky.dart';
 import 'package:flutter_bluesky/screen/base.dart';
 import 'package:flutter_bluesky/screen/provider.dart';
 import 'package:flutter_bluesky/util/base_util.dart';
+import 'package:flutter_bluesky/screen/search.dart';
 import 'package:flutter_bluesky/util/login_util.dart';
 import 'package:flutter_login/flutter_login.dart';
 // ignore: implementation_imports
@@ -153,7 +154,11 @@ class LoginScreen extends StatelessWidget {
 
   void view(BuildContext context) {
     // below code store login state at restart.
-    pushBase(context);
+    if (plugin.api.session.actor!.followsCount == 0) {
+      pushBase(context, selectedIndex: searchIndex);
+    } else {
+      pushBase(context);
+    }
   }
 
   List<TermOfService> terms() {
